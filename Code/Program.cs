c587e67bab3cd6f9f7bce1f90b4b3d1e924f9037
Yusren270 <usreniso@gmail.com>
2024-12-09 13:23:48 +0200

    Add files via upload

---
+++ Code/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = Console.ReadLine();
+            var lastName = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var town = Console.ReadLine();
+            Console.WriteLine("You are" + firstName + " " + lastName + " " + age );
+        }
+    }
+}
---
+++ cikli/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace cikli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i= 'a';i<='j';i
+                  
+            }
+              Console.WriteLine(i);
+            { 
+        }
+    }
+}
